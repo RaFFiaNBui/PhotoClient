0ac7ceb247ae46451ef036741d2adaa1af502e35
RaFFiaNBui <Raffian_bui@mail.ru>
2020-05-31 20:05:07 +0300

    changed moxy strategy

--- app/src/main/java/com/example/photoclient/view/MoxyView.java
+++ app/src/main/java/com/example/photoclient/view/MoxyView.java
@@ -2,11 +2,12 @@ package com.example.photoclient.view;
 
 import moxy.MvpView;
 import moxy.viewstate.strategy.AddToEndSingleStrategy;
+import moxy.viewstate.strategy.SkipStrategy;
 import moxy.viewstate.strategy.StateStrategyType;
 
 public interface MoxyView extends MvpView {
 
-    @StateStrategyType(value = AddToEndSingleStrategy.class)
+    @StateStrategyType(value = SkipStrategy.class)
     void showPicture(String url);
 
     @StateStrategyType(value = AddToEndSingleStrategy.class)
